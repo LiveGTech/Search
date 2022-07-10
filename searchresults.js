263fdcddbf44133a886ac169892107c575c3b5a6
James-Livesey <jamesliveg@gmail.com>
2022-07-10 19:25:31 +0100

    Add basic refined search algorithm

--- searchresults.js
+++ searchresults.js
@@ -7,24 +7,62 @@
     Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 */
 
+export var webIndex = null;
+export var webFuse = null;
+
 export class SearchResult {
-    constructor(url, title, description = "") {
+    constructor(url, title, description = "", weighting = 1) {
         this.url = url;
         this.title = title;
         this.description = description;
+        this.weighting = weighting;
     }
 };
 
-export function getWebResults(query) {
-    return new Promise(function(resolve, reject) {
-        // TODO: This is for testing only at the moment; add proper results fetching
+function normaliseText(text) {
+    return text.trim().toLocaleLowerCase().replace(/[\s'‘’"“”,.!?:;*|\\/\-–—()`]/g, "");
+}
 
-        setTimeout(function() {
-            resolve([
-                new SearchResult("https://liveg.tech", "LiveG Technologies", "We're LiveG Technologies, a group dedicated to developing software and hardware that's made for everyone."),
-                new SearchResult("https://search.liveg.tech", "LiveG Search", "Search the internet for information including articles, websites, imagery and more."),
-                new SearchResult("https://docs.liveg.tech", "LiveG Docs", "The hub for all of LiveG's help guides, technical references and more.")
-            ]);
-        }, 1_000);
+function webIndexOn(object, query) {
+    return Object.keys(object)
+        .filter((key) => key.includes(normaliseText(query)))
+        .map((key) => object[key].map((position) => webIndex.index[position]))
+        .flat()
+    ;
+}
+
+function alterWeightings(results, factor) {
+    return results.map((result) => ({
+        ...result,
+        weighting: result.weighting * factor
+    }));
+}
+
+export function getWebResults(query) {
+    return (webIndex == null ? fetch("indexing/index.json").then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        webIndex = data;
+    }) : Promise.resolve()).then(function() {
+        var results = [
+            ...webIndexOn(webIndex.titles, query),
+            ...alterWeightings(webIndexOn(webIndex.phrases, query), 0.8),
+            ...alterWeightings(webIndexOn(webIndex.descriptions, query), 0.5)
+        ];
+            
+        results = results.map((result) => new SearchResult(
+                result.url,
+                result.title,
+                result.description,
+                result.weighting
+            ))
+            .sort((a, b) => b.weighting - a.weighting)
+        ;
+
+        results = results.filter(function(value, index, self) {
+            return index == self.findIndex((item) => item.url == value.url);
+        })
+
+        return Promise.resolve(results);
     });
 }
